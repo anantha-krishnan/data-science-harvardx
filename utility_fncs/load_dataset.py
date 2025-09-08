2fa9a37d1a8d311f8c0dac8dbfaa274f5c69197e
Ananthakrishnan <anantk@altair.com>
2025-09-08 18:04:24 +0530

    adding more datasets

--- utility_fncs/load_dataset.py
+++ utility_fncs/load_dataset.py
@@ -13,3 +13,27 @@ print("DataFrame 'adv_df' for 'Advertising.csv' has been created in the data_loa
 credit_card_csv_path = os.path.join(data_dir, 'credit.csv')
 credit_df=pd.read_csv(credit_card_csv_path)
 print("DataFrame 'credit_df' for 'credit.csv' has been created in the data_loader module.")
+
+poly_csv_path = os.path.join(data_dir, 'poly.csv')
+poly_df=pd.read_csv(poly_csv_path)
+print("DataFrame 'poly_df' for 'poly.csv' has been created in the data_loader module.")
+
+collinear_csv_path = os.path.join(data_dir, 'colinearity.csv')
+collinear_df=pd.read_csv(collinear_csv_path)
+print("DataFrame 'collinear_df' for 'colinearity.csv' has been created in the data_loader module.")
+
+dataset_csv_path = os.path.join(data_dir, 'dataset.csv')
+dataset_df=pd.read_csv(dataset_csv_path)
+print("DataFrame 'dataset_df' for 'dataset.csv' has been created in the data_loader module.")
+
+noisypopulation_csv_path = os.path.join(data_dir, 'noisypopulation.csv')
+noisypopulation_df=pd.read_csv(noisypopulation_csv_path)
+print("DataFrame 'noisypopulation_df' for 'noisypopulation.csv' has been created in the data_loader module.")
+
+polynomial50_csv_path = os.path.join(data_dir, 'polynomial50.csv')
+polynomial50_df=pd.read_csv(polynomial50_csv_path)
+print("DataFrame 'polynomial50_df' for 'polynomial50.csv' has been created in the data_loader module.")
+
+insurance_claim_csv_path = os.path.join(data_dir, 'insurance_claim.csv')
+insurance_claim_df=pd.read_csv(insurance_claim_csv_path)
+print("DataFrame 'insurance_claim_df' for 'insurance_claim.csv' has been created in the data_loader module.")
